b1bcadc8b24b6b65a27946485f9c1acf62c7893e
acsyom <emichel@acsyom.com>
2026-02-24 20:44:55 -0500

    Updates

---
+++ assets/quotes-48w1IwBU.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-CyWO3JYX.js";const o={async estimate(t){return a.post("/calculator/estimate",t)},async saveContact(t,c){return a.put(`/calculator/${t}/contact`,c)}};export{o as q};
--- assets/quotes-CQ5Ewz-0.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index-BwvgEM7m.js";const o={async estimate(t){return a.post("/calculator/estimate",t)},async saveContact(t,c){return a.put(`/calculator/${t}/contact`,c)}};export{o as q};
